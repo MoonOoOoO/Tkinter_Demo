9c89c6dd77cd7caecb045a23b7cca8e244258f6a
MoonStarCZW <chi32@pnw.edu>
2018-09-12 15:35:55 -0400

    Update

--- demo.py
+++ demo.py
@@ -225,4 +225,16 @@ btn1 = tk.Button(
     command=move_it
 )
 btn1.pack()
+
+frm = tk.Frame(window)
+frm.pack(side='left')
+
+frm_l = tk.Frame(frm)
+frm_l.pack(side='left')
+frm_r = tk.Frame(frm)
+frm_r.pack(side='right')
+
+tk.Label(frm_l, text='on the frm_l1').pack()
+tk.Label(frm_l, text='on the frm_l2').pack()
+tk.Label(frm_r, text='on the frm_r1').pack()
 window.mainloop()
